6328616d0d94d0fde5c14fc0afe97f3ee6414878
yang <729563536@qq.com>
2019-04-05 18:15:41 +0800

    修改配置

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 
-const heapdump = require('heapdump');
-heapdump.writeSnapshot(path.resolve(__dirname + '/log/data' + Date.now() + '.heapsnapshot'));
+// const heapdump = require('heapdump');
+// heapdump.writeSnapshot(path.resolve(__dirname + '/log/data' + Date.now() + '.heapsnapshot'));
 
 const pageRouter = require('./pageRouter');
 const backendApi = require('./interface/backend');
 
 //连接数据库
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true });
-// mongoose.connect('mongodb://localhost:21018/blog', { useNewUrlParser: true });
+const dbPort = process.env.NODE_ENV === 'development' ? 27017 : 21018;
+mongoose.connect(
+  `mongodb://localhost:${dbPort}/blog`,
+  { useNewUrlParser: true }
+);
 mongoose.connection.on('error', console.log.bind(console, 'connection error:'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +24,7 @@ app.use(pageRouter);
 app.use(backendApi);
 app.use('/', express.static(path.join(__dirname + '/static')));
 app.set('port', 8100);
-app.listen(app.get('port'), err => {
+app.listen(process.env.PORT || app.get('port'), err => {
   if (err) throw err;
   console.log('server connect in http://localhost:' + app.get('port'));
 });
